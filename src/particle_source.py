d528d9ee69063d36b23d0e4f215bc9deabd275e6
Felicity Nielson <felicitynielson@felicitys-air.myfiosgateway.com>
2024-03-23 19:55:19 -0400

    injection spray trajectories randomized

--- src/particle_source.py
+++ src/particle_source.py
@@ -11,8 +11,20 @@ def add_particle(x, v, a, dt, box_size, ball_radius, R, m, T):
     # Inject the particles just below the boundary so they don't get stuck.
     source_coords = np.array([[0,box_size-(ball_radius+.001),0]])
 
-    # Start with a simple perfectly straight injection of particles. I will update this with variance.
-    particle_v = np.array([[0, -Vrms, 0]])
+    # # Start with a simple perfectly straight injection of particles. I will update this with variance.
+    # particle_v = np.array([[0, -Vrms, 0]])
+
+    # We want the particles to have some variance in their injection trajectory.
+    # Start by choosing a random velocity in the injection direction/axes and then split the remaining
+    # velocity magnitude between the other two directions/axes.
+    vy = np.random.uniform(-Vrms*0.9, -Vrms*0.7) # Bounds are arbitrarily chosen.
+    temp_v = Vrms - abs(vy)
+    vx = np.random.uniform(-temp_v, temp_v)
+    temp_v -= vx
+    sign = np.random.choice([-1, 1])
+    vz = sign*temp_v
+    
+    particle_v = np.array([[vx, vy, vz]])
     #particle_v = np.random.uniform(low=0.9*Vrms, high=1.1*Vrms, size=(1,3))
     
     # Add particle to particle vector
