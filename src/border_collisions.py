616dc455f908591c16d2f5a342e78bcec66b05e1
Andy Yu <ayu303@gatech.edu>
2024-03-26 22:25:53 -0400

    Consider 3-dimensions for boundary collisions

--- src/border_collisions.py
+++ src/border_collisions.py
@@ -1,37 +1,54 @@
 import taichi as ti
 import numpy as np
+from ideal_gas import box_size
 ti.init(arch=ti.cpu)  # Alternatively, ti.init(arch=ti.cpu), ti.init(arch=ti.vulkan)
 
 #assume rectangular box
-boundary_x = [0, 100] #lower + upper x coords for box
-boundary_y = [0, 100] #lower + upper y coords for box
+boundary_x = [0, box_size] #lower + upper x coords for box
+boundary_y = [0, box_size] #lower + upper y coords for box
+boundary_z = [0, box_size] #lower + upper z coords for box
+
 # @ti.kernel
-def boundary_check(x, y)->int:
+def boundary_check(x, y, z)->int:
     """
     Checks that the particle is not hitting the boundary wall
     x, y: ball center coords
     ball_radius: global size of a particle
     returns 0 if no boundary collision
-    returns 1 if collision with horizontal walls
-    returns 2 if collision with vertical walls
-    returns 3 if collision with both walls (in the case of a corner)
+    returns 1 if collision with x bounds only
+    returns 2 if collision with y bounds only
+    returns 3 if collision with z bounds only
+    returns 4 if collision with x & y bounds (corner)
+    returns 5 if collision with x & z bounds (corner)
+    returns 6 if collision with y & z bounds (corner)
+    returns 7 if collision with x, y & z bounds (corner)
+
     """
     global ball_radius, boundary_x, boundary_y
     hitsXBounds = (x - ball_radius <= boundary_x[0] or boundary_x[1] <= x + ball_radius)
     hitsYBounds = (y - ball_radius <= boundary_y[0] or boundary_y[1] <= y + ball_radius)
+    hitsZBounds = (z - ball_radius <= boundary_z[0] or boundary_z[1] <= z + ball_radius)
 
+    if hitsXBounds & hitsYBounds & hitsZBounds:
+        return 7
+    if hitsYBounds & hitsZBounds:
+        return 6  
+    if hitsXBounds & hitsZBounds:
+        return 5
     if hitsXBounds & hitsYBounds:
-        return 3
+        return 4 
     elif hitsXBounds:
         return 1
     elif hitsYBounds:
         return 2
+    elif hitsZBounds:
+        return 3
 
     return 0
 
 
 # @ti.kernel
-def border_collision(v_x, v_y, x, y, heat_factor):
+def border_collision(v_x, v_y, v_z x, y, z, heat_factor):
     """
     Runs every step of d_t
     v_x, v_y: velocity components
@@ -47,10 +64,20 @@ def border_collision(v_x, v_y, x, y, heat_factor):
         #reverse y velocity; 
         v_y = -1 * heat_factor *  v_y
     elif hitsBounds == 3:
+        #reverse y velocity; 
+        v_z = -1 * heat_factor *  v_z
+    elif hitsBounds == 4:
         #since the increase in velocity is being applied to two components
         #use the square root as a coefficient 
         heat_factor = ti.math.sqrt(heat_factor)
         #reverse both velocities
         v_x = -1 * heat_factor * v_x
         v_y = -1 * heat_factor * v_y
-    return v_x, v_y
+    elif hitsBounds == 5:
+        #since the increase in velocity is being applied to two components
+        #use the square root as a coefficient 
+        heat_factor = ti.math.sqrt(heat_factor)
+        #reverse both velocities
+        v_x = -1 * heat_factor * v_x
+        v_y = -1 * heat_factor * v_y
+    return v_x, v_y, v_z
